9bd59f6ade6e861f359ea79f3761613d89edef7d
Serge Kim <sergevkim@gmail.com>
2022-10-27 17:18:45 +0300

    check images list

--- dataset.py
+++ dataset.py
@@ -10,18 +10,20 @@ class FFHQ_Dataset(Dataset):
     Usage:
         Self-coded class for loading the FFHQ data
     '''
-    
+
     def __init__(self, image_folder, transform = None):
         images_list = os.listdir(image_folder)
         self.images_list = sorted([os.path.join(image_folder, image) for image in images_list])
+        print(len(images_list))
+        print(images_list[:10])
         self.transform = transform
-    
+
     def __getitem__(self, index):
         img_id = self.images_list[index]
         img = Image.open(img_id).convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
-        
+
         return img
 
     def __len__(self):
